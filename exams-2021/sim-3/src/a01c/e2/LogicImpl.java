ea105a60e226bae7b7387755c499e31ebded2704
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-01-20 11:44:43 +0100

    a01c: Finished GUI.

--- exams-2021/sim-3/src/a01c/e2/LogicImpl.java
+++ exams-2021/sim-3/src/a01c/e2/LogicImpl.java
@@ -13,14 +13,11 @@ public class LogicImpl implements Logic {
     @Override
     public void registerMove(int x, int y) {
         if (hasEnoughMoves()) {
-            previousDirection = Optional.of(getDirection(moves.get(0), moves.get(1)));
+            previousDirection = getDirection(moves.get(0), moves.get(1));
             moves.remove(0);
         }
         
         moves.add(new Pair<>(x, y));
-        
-        System.out.println(moves);
-        System.out.println(previousDirection);
     }
 
     @Override
@@ -41,15 +38,27 @@ public class LogicImpl implements Logic {
 
     @Override
     public boolean isValid() {
-        if (previousDirection.isPresent()) {
-            System.out.println(previousDirection.get());
+        if (!previousDirection.isPresent()) {
+            return true;
         }
-        return !previousDirection.isPresent() ? true
-                : previousDirection.get() != getDirection(moves.get(0), moves.get(1));
+
+        var direction = getDirection(moves.get(0), moves.get(1));
+        var condition = direction.isPresent() && previousDirection.get() != direction.get();
+        
+        // If the move is not valid, we need to remove it.
+        if (!condition) {
+            moves.remove(1);
+        }
+        return condition;
     }
 
-    private Direction getDirection(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
-        return p1.getX() == p2.getX() ? Direction.VERTICAL : Direction.HORIZONTAL;
+    private Optional<Direction> getDirection(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
+        if (p1.equals(p2) || (p1.getX() != p2.getX() && p1.getY() != p2.getY())) {
+            return Optional.empty();
+        }
+
+        return p1.getX() == p2.getX() ? Optional.of(Direction.VERTICAL) : Optional.of(Direction.HORIZONTAL);
+
     }
 
     private boolean hasEnoughMoves() {
